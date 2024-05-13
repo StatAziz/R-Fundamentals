055cda1ad1a516ec7a3744f9924caf791a99e152
Azizul Islam <142349067+StatAziz@users.noreply.github.com>
2024-05-13 08:14:48 -0500

    codes on 5/13/24

--- R_Summer2024.R
+++ R_Summer2024.R
@@ -28,4 +28,33 @@ df %>% pull(price)%>% order() # now works as pull() creates vector
 df %>% arrange(desc(price)) # arrange works on dataframe
 df %>% arrange(desc(price)) %>% slice(2) # to find the nth one in dataframe format
 df %>% arrange(desc(price)) %>% slice(2) %>% pull(price) # gives only price
-# ---------------------------------------------------------------------------------------------
+# -----------05/13/2024------------------------------------------------------------------
+#rep functions
+rep(3,3)
+rep(1:3,3)
+rep(NA,3)
+rep(rnorm(2),3)
+
+#sample function
+sample(5)
+sample(1:5)
+a<- c(2,3,4,4,5,3,3,4,3,9,0)
+
+#sample(data, size, replace=T/F)
+sample(a,3, replace= TRUE)
+
+#seq function
+seq(1:8)
+seq(1,20,2.5)
+seq(0,1,0.2)
+
+#replace function
+df<- data.frame(x= c(1,2,3,4), y= c(5,6,7,8))
+new_df<- replace(df$y, df$y>5, 10)
+new_df
+new_df<- replace(df$y, c(2,4), c(100,200))
+new_df
+new_df<- replace(df$y, 3, .9)
+new_df
+
+#-------------------------------------------------------------------------------------------
