f44620b53013f8d3867ffbabb43e4e55cf2d5e24
Azizul Islam <142349067+StatAziz@users.noreply.github.com>
2024-05-18 16:28:30 -0500

    purr::map() function

--- R_Summer2024.R
+++ R_Summer2024.R
@@ -214,4 +214,42 @@ f6 <- function() {
 # Call the function without supplying an argument.
 f6()
 
-#-----------------------------------------------------------------------------------------------------
+#---------------5/18/2024-------------------------------------------------------------------------
+# map function in R from https://www.statology.org/map-function-in-r/
+
+# The map() function from the purrr package in R can be used to apply some function to each element in a vector or list and return a list as a result.
+# 
+# This function uses the following basic syntax:
+#   
+# map(.x, .f)
+# 
+# where:
+#   
+# .x: A vector or list
+# .f: A function
+
+
+library(purrr)
+
+#define vector
+data <- 1:3
+
+#apply rnorm() function to each value in vector
+data %>%
+  map(function(x) rnorm(5, x))
+
+#define vector
+data <- c(2, 4, 10, 15, 20)
+
+#calculate square of each value in the vector
+data %>%
+  map(function(x) x^2)
+#define list of vectors
+data <- list(c(1, 2, 3),
+             c(4, 5, 6),
+             c(7, 8, NA))
+
+#calculate mean value of each vector in list
+data %>%
+  map(mean, na.rm=TRUE)
+#---------------------------------------------------------------------------------------------
